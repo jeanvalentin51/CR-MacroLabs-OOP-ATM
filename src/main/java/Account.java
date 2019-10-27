8b2f6292c448d20bb1049ebaa3b51b798ae2d777
mike <mike.schoenbeck@gmail.com>
2019-10-27 19:05:01 -0400

    created account constructer and getters and setters

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Account {
+    private String type;
+    private String number;
+    private Integer balance;
+
+
+
+    public Account(String type, String number, Integer balance) {
+        this.type = type;
+        this.number = number;
+        this.balance = balance;
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
 }
