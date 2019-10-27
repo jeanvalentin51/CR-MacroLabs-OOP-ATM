a61b6a08af60cd46ab1790812c0929963320c7b3
jeanvalentin51 <valentin.j.gheorghe.com>
2019-10-27 11:30:55 -0400

    Add logic for user profile (set/get/change user)

--- src/test/java/UserProfileTest.java
+++ src/test/java/UserProfileTest.java
@@ -1,7 +1,32 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserProfileTest {
 
+        private UserProfile testUser;
+
+        @Test
+        public void testGetUserPassword(){
+            String expected = "Madskilz";
+            String actual = "";
+            this.testUser = new UserProfile("Madskilz");
+
+            actual = this.testUser.getUserPassword();
+
+            Assert.assertEquals(expected,actual);
+        }
+
+        @Test
+        public void testSetUserPassword(){
+            String expected = "Madskilz";
+            String actual = "";
+            this.testUser = new UserProfile();
+
+            this.testUser.setUserPassword(expected);
+            actual = this.testUser.getUserPassword();
+
+            Assert.assertEquals(expected,actual);
+        }
 
 }
