d5f4c2a8f20d7cbdeb33d56ebb72dab72735b0d5
mike <mike.schoenbeck@gmail.com>
2019-10-30 12:52:45 -0400

    added more methods to account actions

--- src/main/java/AccountActions.java
+++ src/main/java/AccountActions.java
@@ -2,18 +2,30 @@
 
 
 public class AccountActions {
-Account account = new Account();
 
 
-    public Double getBalance() {
+
+    public Double getBalance(Account account) {
         return account.getBalance();
     }
 
-    public Double withdraw(Double withdrawal)    {
+    public Double withdraw(Account account, Double withdrawal)    {
         Double balance = account.getBalance();
         Double newBalance = balance - withdrawal;
         return newBalance;
     }
+
+    public Double deposit(Account account, Double deposit)  {
+        Double balance = account.getBalance();
+        Double newBalance = balance + deposit;
+        return newBalance;
+    }
+
+    public Double checkCurrentBalance(Account account)  {
+        return account.getBalance();
+    }
+
+
 
 
 
--- src/main/java/AccountRepository.java
+++ src/main/java/AccountRepository.java
@@ -1,5 +1,8 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class AccountRepository {
+
+    private volatile Map<String, AccountRepository> accountRepository = new HashMap<>();
 
 }
